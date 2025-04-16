a21f8dda940dd1882652e15f0e9a5b15955f6be0
Fahmy RABIE <fahmy.rabie@intalio.com>
2025-04-16 14:24:39 +0200

    fm

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using NewUmbraco.Configuration;
+
+using Slimsy.DependencyInjection;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddSlimsy()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.Configure<NewUmbracoConfig>(
+    builder.Configuration.GetSection(NewUmbracoConfig.SectionName));
+
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
