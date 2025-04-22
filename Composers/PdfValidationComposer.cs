7f68284a6f0f576309fdeb12cad5d3921c3afe11
Fahmy Gharbia <121654791+fahmygharbia@users.noreply.github.com>
2025-04-22 11:29:11 +0200

    New feature (#1)
    * Add components about training screen
    
    * uSync
    
    ---------
    
    Co-authored-by: Fahmy RABIE <fahmy.rabie@intalio.com>

---
+++ Composers/PdfValidationComposer.cs
@@ -0,0 +1,11 @@
+﻿//using Umbraco.Cms.Core.Composing;
+//using Umbraco.Cms.Core.Notifications;
+
+//public class PdfValidationComposer : IComposer
+//{
+//    public void Compose(IUmbracoBuilder builder)
+//    {
+//        builder.AddNotificationHandler<MediaSavingNotification, ValidatePdfUploadHandler>();
+//    }
+//}
+
