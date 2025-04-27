c8e803f6a5a5ab30135b6e46a37dfec97e661eb6
Fahmy RABIE <fahmy.rabie@intalio.com>
2025-04-27 10:26:50 +0300

    Pull new changes

---
+++ Services/ISiteService.cs
@@ -0,0 +1,9 @@
+﻿using NewUmbraco.Models.ViewModels;
+
+namespace NewUmbraco.Services;
+
+public interface ISiteService
+{
+    IEnumerable<MainSidebarModel> GetSideBarChildNodes();
+    PageContent GetContent(int id);
+}
